19367f9e6ea32666cd2ed8e68d18b0ba1faf797f
poonasp257 <poonasp257@gmail.com>
2019-09-20 02:09:07 +0900

    Update
    1. 클라이언트
    - 마을 맵 구성을 위해 오브젝트 배치
    - Playerable 캐릭터 이동 애니메이션 및 Following camera 구현

---
+++ Client/Heroes/Assets/Heroes/Scripts/CameraFollow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public GameObject cameraFollowObj;
+    public float cameraMoveSpeed = 120.0f;
+    public float clampAngle = 80.0f;
+    public float inputSensitivity = 150.0f;
+    public float mouseX;
+    public float mouseY;
+    private Vector3 rotation;
+
+    // Start is called before the first frame update
+    private void Start() {
+        rotation = transform.localRotation.eulerAngles;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+
+        rotation.y += mouseX * inputSensitivity * Time.deltaTime;
+        rotation.x += mouseY * inputSensitivity * Time.deltaTime;
+
+        rotation.x = Mathf.Clamp(rotation.x, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotation.x, rotation.y, 0.0f);
+        transform.rotation = localRotation;
+    }
+
+    void LateUpdate() {
+        CameraUpdater();
+    }
+
+    void CameraUpdater() {
+        Transform target = cameraFollowObj.transform;
+        float step = cameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
---
+++ Client/Heroes/Assets/Heroes/Scripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Animator animator;
+    private CharacterController controller;
+
+    private float inputX;
+    private float inputZ;
+    private float gravity;
+    private Vector3 desiredMoveDirection;
+
+
+    public float desiredRotationSpeed;
+    public float allowPlayerRotation;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start() {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        inputX = Input.GetAxis("Horizontal");
+        inputZ = Input.GetAxis("Vertical");
+
+        ProcessInput();
+    }
+
+    void ProcessInput() {
+        animator.SetFloat("InputX", inputX, 0.0f, Time.deltaTime * 2.0f);
+        animator.SetFloat("InputZ", inputZ, 0.0f, Time.deltaTime * 2.0f);
+
+        float inputMagnitude = new Vector2(inputX, inputZ).sqrMagnitude;
+
+        if (inputMagnitude > allowPlayerRotation) Rotation();
+        else desiredMoveDirection = Vector3.zero;
+
+        Move();
+    }
+
+    void Rotation() {
+        Camera camera = Camera.main;
+        Vector3 forward = camera.transform.forward;
+        Vector3 right = camera.transform.right;
+
+        forward.y = 0.0f;
+        right.y = 0.0f;
+
+        forward.Normalize();
+        right.Normalize();
+
+        desiredMoveDirection = forward * inputZ + right * inputX;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation,
+        Quaternion.LookRotation(desiredMoveDirection), desiredRotationSpeed);
+    }
+
+    void Move() {
+        gravity -= 9.8f * Time.deltaTime;
+
+        Vector3 moveDirection = desiredMoveDirection * moveSpeed * Time.deltaTime;
+        moveDirection = new Vector3(moveDirection.x, gravity, moveDirection.z);
+        controller.Move(moveDirection);
+
+        if(controller.isGrounded) gravity = 0;
+    }
+}
