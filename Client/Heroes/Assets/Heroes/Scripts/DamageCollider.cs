9a6249fcb71caa81c408552f13fdb64667ff296c
poonasp257 <poonasp257@naver.com>
2019-09-30 17:00:13 +0900

    Update
    # Client
    1. Weapon Control

--- Client/Heroes/Assets/Heroes/Scripts/DamageCollider.cs
+++ Client/Heroes/Assets/Heroes/Scripts/DamageCollider.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 namespace Heroes {
 	public class DamageCollider : MonoBehaviour {
+		private BoxCollider collider;
+
+		private void Start() {
+			collider = GetComponent<BoxCollider>();
+			if (collider) collider.enabled = false;
+		}
+
 		private void OnTriggerEnter(Collider other) {
 			EnemyStateManager enemyState = other.GetComponent<EnemyStateManager>();
 
 			if (enemyState == null) return;
 
 			enemyState.TakeDamage(25);
+		}
+
+		public void Enable() {
+			if (collider == null) return;
+
+			collider.enabled = true;
+		}
+
+		public void Disable() {
+			if (collider == null) return;
+			
+			collider.enabled = false;
 		}
 	}
 }
---
+++ Client/Heroes/Assets/WeaponController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Heroes {
+	public class WeaponController : MonoBehaviour {
+		[SerializeField] private DamageCollider[] colliders;
+
+		private void Start() {
+			colliders = GetComponentsInChildren<DamageCollider>();
+			DisableDamageColliders();
+		}
+
+		public void EnableDamageColliders() {
+			foreach(DamageCollider damageCollider in colliders) {
+				damageCollider.Enable();
+			}
+		}
+
+		public void DisableDamageColliders() {
+			foreach(DamageCollider damageCollider in colliders) {
+				damageCollider.Disable();
+			}
+		}
+	}
+}
