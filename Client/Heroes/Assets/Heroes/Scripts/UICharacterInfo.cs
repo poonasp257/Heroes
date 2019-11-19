9d106c9e474feb54b16f0d86226080831e393105
poonasp257 <poonasp257@gmail.com>
2019-11-19 19:02:19 +0900

    Update
    # Client
    캐릭터 선택창 구현 중..

---
+++ Client/Heroes/Assets/Heroes/Scripts/UICharacterInfo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace Heroes {
+	public class UICharacterInfo : MonoBehaviour {
+		private Image classIcon;
+		private Text level;
+		private Text name;
+		private Text location;
+		private Button connectButton;
+
+		private void Start() {
+			
+		}
+
+		private void Update() {
+
+		}
+	}
+}
