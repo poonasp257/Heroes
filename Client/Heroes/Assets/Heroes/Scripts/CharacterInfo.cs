c0a69f76f24dbba66be9dab8b7c6bc74bf94dba1
poonasp257 <poonasp257@naver.com>
2019-11-19 13:52:52 +0900

    Backup

---
+++ Client/Heroes/Assets/Heroes/Scripts/CharacterInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Heroes {
+	public class CharacterInfo : MonoBehaviour {
+		private Image 
+
+		private level, name, location, classIcon;
+
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
